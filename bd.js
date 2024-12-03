6b29529582e6398377cc5562c31ea8cf6c4c39bc
lady <165735018+ledaSantos@users.noreply.github.com>
2024-12-03 19:08:59 +0000

    kk

--- bd.js
+++ bd.js
@@ -28,3 +28,36 @@ async function selectUsuario(id) {
 }
 
 export { selectUsuarios, selectUsuario };
+
+async function insertUsuario(data) {
+  const client = await connect();
+  const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
+  const usuario = [data.nome, data.senha, data.email];
+  await client.query(query, usuario);
+  client.release();
+}
+
+export { selectUsuarios, selectUsuario, insertUsuario };
+
+//bd.js
+async function deleteUsuario(id) {
+  const client = await connect();
+  const query = "DELETE FROM usuario WHERE id = $1";
+  await client.query(query, [id]);
+  client.release();
+}
+
+//bd.js
+export { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario };
+
+//bd.js
+async function updateUsuario(id, data) {
+  const client = await connect();
+  const query = "UPDATE usuario SET nome = $1, email = $2, senha = $3 WHERE id = $4";
+  const usuario = [data.nome, data.email, data.senha, id];
+  await client.query(query, usuario);
+  client.release();
+}
+
+//bd.js
+export { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario };
