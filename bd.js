bfbe07f5a1b5e0e9e8462dbb274dbe22ecab5a1a
lady <165735018+ledaSantos@users.noreply.github.com>
2024-12-03 18:48:15 +0000

    jj

--- bd.js
+++ bd.js
@@ -16,3 +16,15 @@ async function selectUsuarios() {
 }
 
 export { selectUsuarios };
+
+//bd.js
+async function selectUsuario(id) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE id = $1";
+  const usuario = [id];
+  const res = await client.query(query, usuario);
+  client.release();
+  return res.rows;
+}
+
+export { selectUsuarios, selectUsuario };
--- index.js
+++ index.js
@@ -31,3 +31,18 @@ app.listen(port, () => {
   // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
+
+//index.js
+import { selectUsuarios, selectUsuario } from "./bd.js";
+
+//index.js
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario/# solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
